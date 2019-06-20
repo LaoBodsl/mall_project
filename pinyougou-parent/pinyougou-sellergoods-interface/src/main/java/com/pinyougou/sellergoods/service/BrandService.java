dbd2712a91e4e896fc8b2744b4cfd23bcf2dc064
LaoBodsl <laobodslr@gmail.com>
2019-06-20 09:20:45 +0800

    finish_brand_manager

--- pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResult;
+
 /**
  * 品牌接口
  * @author Administrator
@@ -13,4 +15,13 @@ public interface BrandService {
 
 	public List<TbBrand> findAll();
 	
+	public PageResult findPage(int pageNum,int pageSize);
+	
+	public void add(TbBrand brand);
+	public void update(TbBrand brand);//修改
+	public TbBrand findOne(Long id);//获取id
+	public void delete(Long [] ids);
+	public PageResult findPage(TbBrand brand,int pageNum,int pageSize);
 }
+
+
