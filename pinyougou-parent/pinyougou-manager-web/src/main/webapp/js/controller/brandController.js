fc8a401fa4df746ab911179b243f1328e2aee183
LaoBodsl <laobodslr@gmail.com>
2019-06-21 14:29:56 +0800

    fixbug

---
+++ pinyougou-parent/pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
@@ -0,0 +1,69 @@
+app.controller('brandController',function($scope,$controller,brandService){
+			$controller('baseController',{$scope:$scope});//继承
+			//查询品牌列表
+			$scope.findAll=function(){
+				alert("sdfasdf")
+				brandService.findAll().success(
+					function(response){
+						$scope.list=response;
+						
+					}		
+				);				
+			}
+			
+			//分页
+			$scope.findPage=function(page,size){
+				brandService.findPage(page,size).success(
+					function(response){
+						$scope.list = response.rows;
+						$scope.paginationConf.totalItems=response.total;//更新总记录数
+					}
+				);
+			}
+			//保存
+			$scope.save=function(){
+				var object = null;
+				if($scope.entity.id!=null){
+					object=brandService.update($scope.entity);
+				}else{
+					object=brandService.add($scope.entity);
+				}
+				object.success(
+					function(response){
+						if(response.success){
+							$scope.reloadList();
+						}else{
+							alert(response.message);
+						}
+					}
+				);
+			}
+			$scope.findOne=function(id){
+				brandService.findOne(id).success(
+					function(respinse){
+						$scope.entity=response;
+					}
+				);
+			}
+			
+			//批量删除
+			$scope.dele=function(){
+				brandService.dele($scope.selectIds).success(
+					function(response){
+						if(response.success){
+							$scope.reloadList();
+						}
+					}
+				);
+			}
+			$scope.searchEntity={};
+			$scope.search=function(page,size){
+				brandService.search(page,size,$scope.searchEntity).success(
+					function(response){
+						$scope.list=response.rows;
+						$scope.paginationConf.totalItems=response.total;
+						
+					}
+				);
+			}
+		})
